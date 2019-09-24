5306d2251463fb6485e3c4a8386b8d68e3c5415d
Prabhu Ramamoorthy <rrprabhu@gmail.com>
2019-09-24 16:17:16 -0400

    testvspublic

---
+++ .vscode-server/bin/f06011ac164ae4dc8e753a3fe7f9549844d15e35/out/vs/server/uriTransformer.js
@@ -0,0 +1,5 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+module.exports=function(e){return{transformIncoming:e=>"vscode-remote"===e.scheme?{scheme:"file",path:e.path}:"file"===e.scheme?{scheme:"vscode-local",path:e.path}:e,transformOutgoing:o=>"file"===o.scheme?{scheme:"vscode-remote",authority:e,path:o.path}:"vscode-local"===o.scheme?{scheme:"file",path:o.path}:o,transformOutgoingScheme:e=>"file"===e?"vscode-remote":"vscode-local"===e?"file":e}};
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/f06011ac164ae4dc8e753a3fe7f9549844d15e35/core/vs/server/uriTransformer.js.map
